c2b57242125a7c239b839368cf5f299a953efbd9
Yan-Brav <yan.braverman37@gmail.com>
2020-05-11 18:28:42 +0300

    2020_05_11 slide in right panel has been improved

--- public/js/slide.js
+++ public/js/slide.js
@@ -4,11 +4,15 @@ window.addEventListener('load', function () {
     const saveButton = document.querySelector('#submit');
     const slideForm = document.querySelector('.slide');
     $(newTask).one('click', function () {
-        saveButton.innerText = 'CREATE TASK';
-        $(slideForm).animate({right: '+=1450px'}, 750);
+        if ($('.slide').css('right') === '-1500px') {
+            saveButton.innerText = 'CREATE TASK';
+            $(slideForm).animate({right: '+=1450px'}, 750);
+        }
     });
     $(editButton).one('click', function () {
-        saveButton.innerText = 'EDIT TASK';
-        $(slideForm).animate({right: '+=1450px'}, 750);
+        if ($('.slide').css('right') === '-1500px') {
+            saveButton.innerText = 'EDIT TASK';
+            $(slideForm).animate({right: '+=1450px'}, 750);
+        }
     });
 });
