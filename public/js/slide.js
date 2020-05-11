33c81e5c4206f24be8dbfeb7f4861c863497c321
Yan-Brav <yan.braverman37@gmail.com>
2020-05-11 16:17:53 +0300

    2020_05_11 slide in right panel has been improved by method one instead on

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
     const newTask = document.querySelector("#add_new_task p");
     const slideForm = document.querySelector('.slide');
-    $(newTask).on('click', function () {
-        $(slideForm).animate({right: '+=1350px'}, 500);
+    $(newTask).one('click', function () {
+        $(slideForm).animate({right: '+=1450px'}, 750);
     });
 });
