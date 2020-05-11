fd48b4f9093c28f4cc1de3ad7d81ebb7b217d720
Yan-Brav <yan.braverman37@gmail.com>
2020-05-11 16:52:26 +0300

    2020_05_11 slide in right panel has been implemented by press edit button

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,7 +1,14 @@
 window.addEventListener('load', function () {
     const newTask = document.querySelector("#add_new_task p");
+    const editButton = document.querySelector('.edit');
+    const saveButton = document.querySelector('#submit');
     const slideForm = document.querySelector('.slide');
     $(newTask).one('click', function () {
+        saveButton.innerText = 'CREATE TASK';
+        $(slideForm).animate({right: '+=1450px'}, 750);
+    });
+    $(editButton).one('click', function () {
+        saveButton.innerText = 'EDIT TASK';
         $(slideForm).animate({right: '+=1450px'}, 750);
     });
 });
